87733c0873a1863de05c41a44beb008e2e90d21b
Maryinfun <motmaryion@gmail.com>
2023-01-10 19:34:18 +0300

    init: test 1 done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = n.toString();
+    s = "";
+    if (arr[0] === "-") {
+      arr = arr.slice(1);
+    }
+    i = arr.length - 1;
+    while (i >= 0) {
+      s += arr[i];
+      i--;
+    }
+    return Number(s);
+};
